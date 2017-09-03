0602c8c97d2b73d6d8bcd4edbe84b5bf01bb1000
Vitaliy Platonov <alonestranger@gmail.com>
2017-09-03 04:22:06 +0300

    Добавлена заготовка для сервиса

---
+++ TBotService/TBotSrv.cs
@@ -0,0 +1,66 @@
+﻿using BotLibrary;
+using System;
+using System.ServiceProcess;
+using System.Threading;
+using Telegram.Bot;
+
+namespace TBotService
+{
+    public partial class TBotSrv : ServiceBase
+    {
+        private string _key = "";
+        private bool Working = false;
+        public TBotSrv(string key)
+        {
+            InitializeComponent();
+            _key = key;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (String.IsNullOrEmpty(_key))
+                return;
+
+            try
+            {
+                Main.Bot = new TelegramBotClient(_key); // инициализируем API
+            }
+            catch
+            {
+                return;
+            }
+
+            Working = true;
+            
+            //Стартуем бота
+            DoWork();
+
+            while (Working)
+                Thread.Sleep(100);
+        }
+
+
+
+        protected override void OnStop()
+        {
+            Working = false;
+        }
+
+        /// <summary>
+        /// Задаем методы обрабатывающие события бота и стартуем
+        /// </summary>
+        static async void DoWork()
+        {
+            try
+            {
+                await Main.Bot.SetWebhookAsync("");
+
+                Main.Bot.OnInlineQuery += DoInlineQuery.Proceed;
+                Main.Bot.OnCallbackQuery += DoCallBackQuery.Proceed;
+                Main.Bot.OnUpdate += DoMessage.Proceed;
+                Main.Bot.StartReceiving();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
